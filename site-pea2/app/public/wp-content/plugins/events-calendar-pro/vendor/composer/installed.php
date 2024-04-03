9d7d08ce9bdfad2cbf69d6356de80e98bc61e056
Joel <joelhassam@gmail.com>
2024-04-03 16:42:15 +0000

    Configuration et Paramétrage des réglages de base

---
+++ site-pea2/app/public/wp-content/plugins/events-calendar-pro/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/events-pro',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '906f7f5d6973aa8c95c2a7c48d87de9f06b546b1',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'rlanvin/php-rrule' => array(
+            'pretty_version' => 'v1.6.3',
+            'version' => '1.6.3.0',
+            'reference' => '1373df401e0926cf7548716dabbc0d19603a144d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rlanvin/php-rrule',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'the-events-calendar/events-pro' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '906f7f5d6973aa8c95c2a7c48d87de9f06b546b1',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
